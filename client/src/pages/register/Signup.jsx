5695c73f0b3fd473ef9d69dc8731a229c372eb27
karamoko <arkaramoko@cegid.com>
2022-09-26 13:44:12 +0200

    client

--- client/src/pages/register/Signup.jsx
+++ client/src/pages/register/Signup.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <form>
+        <div class="pseudo">
+          <label> Pseudo : </label>
+          <input
+            aria-label="tapez votre pseudo"
+            type="text"
+            placeholder="Colibri"
+          />
+        </div>
+        <div class="email">
+          <label> Email : </label>
+          <input
+            aria-label="tapez votre email"
+            type="email"
+            placeholder="AMi@FOREVER.COM"
+          />
+        </div>
+        <div class="password">
+          <label> Password : </label>
+          <input aria-label="tapez votre password" type="password" />
+        </div>
+      </form>
+      <div className="ImageSignup">
+        
+      </div>
+    </div>
+  );
 }
